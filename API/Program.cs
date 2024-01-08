9d7f867dfb3a3b3ce8c31a9f8da42fe23553056c
Victor <jvcoliveira2002@gmail.com>
2024-01-08 13:16:47 -0300

    configurando angula

--- API/Program.cs
+++ API/Program.cs
@@ -11,12 +11,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-
+//builder.Services.AddCors();
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+//app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http:/localhost:4200"));
 
 app.UseHttpsRedirection();
 
