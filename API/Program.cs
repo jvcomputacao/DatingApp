a8d34a7cd930d3a7e98a99834c71d7c912b9dd19
Victor <jvcoliveira2002@gmail.com>
2024-01-13 12:38:24 -0300

    login and register

--- API/Program.cs
+++ API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddIdemtityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 //app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http:/localhost:4200"));
 
 app.UseHttpsRedirection();
